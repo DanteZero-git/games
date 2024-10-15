d4e7aea55eceba9fc86797040237059531458028
DanteZero-git <ajsantos.082@hotmail.com>
2024-10-15 02:17:50 +0200

    add: login - corregida funcionalidad contador, funcionalidad 3d y aviso contador validado

--- login/js/login-signup.js
+++ login/js/login-signup.js
@@ -4,29 +4,51 @@ const usuario = document.getElementById("usuario")
 const contraseña = document.getElementById("contraseña")
 
 // Dibuja y mueve el contador segun el minLength del input
-function contadorInput(input, contador) {
-    const numeroCaracteres = input.value.length
+let varExt
+async function contadorInput(input, contador) {
+    const caracteres = input.value.length
     let caracteresMin = input.minLength
-    console.log(numeroCaracteres)
 
-    if (numeroCaracteres <= caracteresMin || input.classList.contains("numeroContador")) {
-        contador.classList.remove("numeroContador", "validado")
+    if (caracteres <= caracteresMin) {
+        contador.classList.remove("validado")
+        contador.innerHTML = caracteresMin - caracteres
+        contador.style.transform = `translateX(${(200 / caracteresMin) * caracteres}px)`
+        clearTimeout(varExt)
+    }
+    if (caracteres === caracteresMin) {
+        contador.style.transform = `translateX(${(200 / caracteresMin) * caracteresMin}px)`
 
-        contador.innerHTML = caracteresMin - numeroCaracteres
-        contador.style.transform = `translateX(${(202 / caracteresMin) * numeroCaracteres}px)`
-        if (numeroCaracteres === caracteresMin) { setTimeout(function() {contador.classList.add("numeroContador", "validado")},500) }
-    } 
-    else {
-        contador.classList.add("numeroContador")
-        contador.innerHTML = 0 
-        contador.style.transform = `translateX(${(202 / caracteresMin) * caracteresMin}px)`
+        varExt = setTimeout(function () {
+            contador.innerHTML = ""
+            contador.classList.add("validado")
+            setTimeout(function () {
+                contador.innerHTML =
+                    `<img src="../../resources/icons/formValidado.png" alt="Correcto" 
+                    style="width: 20px; height: 20px; margin-right: 20px; vertical-align: middle;"> 
+                    Válido`
+            }, 500)
+        }, 1200)
     }
 }
 
+// espera a la carga de la pagina para ejecutar por primera vez las funciones y añadir la clase de transiciones
 window.onload = function () {
     contadorInput(usuario, contador1);
     contadorInput(contraseña, contador2);
-}
+    Array.from(document.getElementsByClassName("fieldset")).forEach(function (item) { item.classList.add("domCargado") })
 
+}
+// eventos de 3d on click
+if (usuario.value) { usuario.parentNode.classList.add("fieldset3d") }
+usuario.parentNode.addEventListener("click", function () {
+    usuario.parentNode.classList.add("fieldset3d")
+    usuario.focus()
+})
+
+contraseña.parentNode.addEventListener("click", function () {
+    contraseña.parentNode.classList.add("fieldset3d")
+    contraseña.focus()
+})
+// eventos de escritura en input
 usuario.addEventListener('input', function () { contadorInput(usuario, contador1) })
 contraseña.addEventListener('input', function () { contadorInput(contraseña, contador2) })
