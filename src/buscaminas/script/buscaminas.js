d766ea7dff267a1f80d0d44eff400463163424d8
DanteZero-git <ajsantos.082@hotmail.com>
2024-10-25 12:23:43 +0200

    added: buscaminas con logica de creacion de bombas

--- src/buscaminas/script/buscaminas.js
+++ src/buscaminas/script/buscaminas.js
@@ -1,46 +1,95 @@
 const cantidad = document.getElementById("cantidad")
 const valorCantidad = document.getElementById("valorCantidad")
 const contenedorGrid = document.getElementById("contenedorGrid")
+const ramdom = 90 // es porcentaje
 
 let valoresCuadricula = []
 for (let i = 20; i <= 30; i++) {
-    valoresCuadricula += (i < 30) ?  `${i}, ` : `${i}`
+    valoresCuadricula += (i < 30) ? `${i}, ` : `${i}`
 }
 
-console.log(valoresCuadricula)
-function dibujarGrid (par) {
+function dibujarGrid(par) {
     contenedorGrid.innerHTML = ""
     const grid = document.createElement("ul")
-    console.log(contenedorGrid.offsetWidth + " - " + contenedorGrid.offsetHeight)
 
-    grid.style.border = "1px solid blue"
-    grid.style.width = "100%" 
-    grid.style.height = "100%" 
-
+    grid.style.width = "100%"
+    grid.style.height = "100%"
     grid.style.display = "grid"
     grid.style.gridTemplateColumns = `repeat(${par}, 1fr)`
     grid.style.gap = "2px"
     grid.style.overflow = "auto"
+    grid.style.scrollbarWidth = "none"
 
     for (let i = 0; i < par * par; i++) {
+        const li = document.createElement("li")
+        grid.appendChild(li)
+        li.classList.add("li")
+        li.id = `casilla${i}`
+        li.innerHTML = `${i}`
 
-    const li = document.createElement("li")
-    grid.appendChild(li)
-
-    li.style.height = "100%"
-    li.style.aspectRatio = "1/1"
-    li.style.backgroundColor = "grey"
-    li.innerHTML = `${i  + 1}`
-    li.style.fontSize = "10px"
-
+        const check = document.createElement("input")
+        li.appendChild(check)
+        check.classList.add("check")
+        check.type = "checkbox"
     }
     contenedorGrid.appendChild(grid)
+    document.head.appendChild(clases)
 }
 
+const clases = document.createElement("style")
+clases.textContent += `
+    .li {
+        position: relative;
+        height: 100%;
+        aspect-ratio: 1/1;
+        border: 1px solid grey;
+        border-radius: 12%;
+        font-size: 10px;
+    }
+
+    .check {
+        all: unset;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+    }
+`
+
+let bombas = []
+function ramdomBombas() {
+    const num = Array.from(contenedorGrid.querySelectorAll("li")).length
+    const cantidadBombas = Math.floor((num * ramdom) / 100)
+
+    while (bombas.length < cantidadBombas) {
+        const numRamdom = Math.floor(Math.random() * num)
+        const item = document.getElementById("casilla" + numRamdom)
+        if (!bombas.includes(numRamdom)) { bombas.push(item) }
+    }
+}
+
+function crearBombas() {
+    bombas.forEach(element => {
+        element.style.background = "grey"
+    })
+}
+
+valorCantidad.innerHTML = cantidad.value * cantidad.value
 dibujarGrid(cantidad.value)
+ramdomBombas()
+crearBombas()
+
 
 cantidad.addEventListener("input", () => {
     valorCantidad.innerHTML = cantidad.value * cantidad.value
     dibujarGrid(cantidad.value)
+    crearBombas()
 })
 
+/* listarCasillas().forEach(element => {
+    element.addEventListener("click", function () {
+        // comparar elementos check con array de elementos bomba
+    })
+}) */
