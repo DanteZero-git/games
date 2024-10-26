5a91bcdfcb44cbb571aa72b4a1e4e28a65886d70
DanteZero-git <ajsantos.082@hotmail.com>
2024-10-26 02:11:43 +0200

    added: buscaminas con logica de deteccion de bombas

--- src/buscaminas/script/buscaminas.js
+++ src/buscaminas/script/buscaminas.js
@@ -1,13 +1,16 @@
 const cantidad = document.getElementById("cantidad")
-const valorCantidad = document.getElementById("valorCantidad")
+const valorCantidad = Number(cantidad.value)
+const valorTextarea = document.getElementById("valorTextarea")
 const contenedorGrid = document.getElementById("contenedorGrid")
-const ramdom = 90 // es porcentaje
+const ramdom = 40 // es porcentaje
 
+// valores min y max del input
 let valoresCuadricula = []
 for (let i = 20; i <= 30; i++) {
     valoresCuadricula += (i < 30) ? `${i}, ` : `${i}`
 }
 
+// dibujar grid
 function dibujarGrid(par) {
     contenedorGrid.innerHTML = ""
     const grid = document.createElement("ul")
@@ -24,7 +27,7 @@ function dibujarGrid(par) {
         const li = document.createElement("li")
         grid.appendChild(li)
         li.classList.add("li")
-        li.id = `casilla${i}`
+        li.id = `${i}`
         li.innerHTML = `${i}`
 
         const check = document.createElement("input")
@@ -58,38 +61,185 @@ clases.textContent += `
     }
 `
 
+// reconocer casillas
+function listarCasillas() {
+    return Array.from(contenedorGrid.querySelectorAll("li"))
+}
+
+// ramdomiza las bombas
 let bombas = []
 function ramdomBombas() {
-    const num = Array.from(contenedorGrid.querySelectorAll("li")).length
-    const cantidadBombas = Math.floor((num * ramdom) / 100)
-
+    const casillas = listarCasillas()
+    const cantidadBombas = Math.floor((casillas.length * ramdom) / 100)
     while (bombas.length < cantidadBombas) {
-        const numRamdom = Math.floor(Math.random() * num)
-        const item = document.getElementById("casilla" + numRamdom)
+        const numRamdom = Math.floor(Math.random() * casillas.length)
+        const item = document.getElementById(numRamdom)
         if (!bombas.includes(numRamdom)) { bombas.push(item) }
+    }
+    bombas.sort((a, b) => Number(a.id) - Number(b.id)) // array ordenado por id
+}
+
+// dibuja las bombas
+function dibujarBombas() {
+    bombas.forEach(element => {
+        element.style.background = "red"
+        element.classList.add("bomba")
+    })
+}
+
+// detecta fila y columna de casilla marcada
+let filas = []
+let columnas = []
+let fila
+let columna
+
+function localizar(item) {
+    item.style.border = "5px solid green"
+    // casillas por fila
+    for (let a = 0; a <= (valorCantidad - 1); a++) {
+        filas[a] = []
+        for (let b = 0; b < valorCantidad; b++) {
+            filas[a].push(`${b + (a * valorCantidad)}`)
+        }
+    }
+    // casillas por columna
+    for (let a = 0; a <= (valorCantidad - 1); a++) {
+        columnas[a] = []
+        for (let b = 0; b < valorCantidad; b++) {
+            columnas[a].push(`${a + (b * valorCantidad)}`)
+        }
+    }
+    // encontrar fila y columna de seleccionado
+    for (let i = 0; i < valorCantidad; i++) {
+        if (filas[i].includes(item.id)) {
+            fila = i
+        }
+        if (columnas[i].includes(item.id)) {
+            columna = i
+        }
     }
 }
 
-function crearBombas() {
-    bombas.forEach(element => {
-        element.style.background = "grey"
-    })
+let casillasAbiertas = []
+function dibujarNum(fila, columna, filas, columnas, casillaAbierta) {
+    if (!casillasAbiertas.includes(casillaAbierta)) {
+        const input = casillaAbierta.children[0]
+        const border = "4px solid red"
+
+        if (!casillaAbierta.classList.contains("bomba")) {
+            // misma fila
+            if (columna > 0) {
+                const izqCentro = document.getElementById(Number(casillaAbierta.id) - 1)
+                if (!izqCentro.classList.contains("bomba")) {
+                    izqCentro.style.border = border
+                    izqCentro.classList.add("contieneNum")
+                } else {
+                    izqCentro.style.backgroundColor = "grey"
+                }
+            }
+            if (columna < valorCantidad - 1) {
+                const derCentro = document.getElementById(Number(casillaAbierta.id) + 1)
+                if (!derCentro.classList.contains("bomba")) {
+                    derCentro.style.border = border
+                    derCentro.classList.add("contieneNum")
+                } else {
+                    derCentro.style.backgroundColor = "grey"
+                }
+            }
+            // fila superior
+            if (fila > 0 && columna > 0) {
+                const arIzq = document.getElementById(Number(casillaAbierta.id) - valorCantidad - 1)
+                if (!arIzq.classList.contains("bomba")) {
+                    arIzq.style.border = border
+                    arIzq.classList.add("contieneNum")
+                } else {
+                    arIzq.style.backgroundColor = "grey"
+                }
+            }
+            if (fila > 0) {
+                const arCen = document.getElementById(Number(casillaAbierta.id) - valorCantidad)
+                if (!arCen.classList.contains("bomba")) {
+                    arCen.style.border = border
+                    arCen.classList.add("contieneNum")
+                } else {
+                    arCen.style.backgroundColor = "grey"
+                }
+            }
+            if (fila > 0 && columna < valorCantidad - 1) {
+                const arDer = document.getElementById(Number(casillaAbierta.id) - valorCantidad + 1)
+                if (!arDer.classList.contains("bomba")) {
+                    arDer.style.border = border
+                    arDer.classList.add("contieneNum")
+                } else {
+                    arDer.style.backgroundColor = "grey"
+                }
+            }
+            // fila inferior
+            if (fila < valorCantidad - 1 && columna > 0) {
+                const abIzq = document.getElementById(Number(casillaAbierta.id) + valorCantidad - 1)
+                if (!abIzq.classList.contains("bomba")) {
+                    abIzq.style.border = border
+                    abIzq.classList.add("contieneNum")
+                } else {
+                    abIzq.style.backgroundColor = "grey"
+                }
+            }
+            if (fila < valorCantidad - 1) {
+                const abCen = document.getElementById(Number(casillaAbierta.id) + valorCantidad)
+                if (!abCen.classList.contains("bomba")) {
+                    abCen.style.border = border
+                    abCen.classList.add("contieneNum")
+                } else {
+                    abCen.style.backgroundColor = "grey"
+                }
+            }
+            if (fila < valorCantidad - 1 && columna < valorCantidad - 1) {
+                const abDer = document.getElementById(Number(casillaAbierta.id) + valorCantidad + 1)
+                if (!abDer.classList.contains("bomba")) {
+                    abDer.style.border = border
+                    abDer.classList.add("contieneNum")
+                } else {
+                    abDer.style.backgroundColor = "grey"
+                }
+            }
+        } else {
+            console.log("bomba")
+        }
+    }
 }
 
-valorCantidad.innerHTML = cantidad.value * cantidad.value
-dibujarGrid(cantidad.value)
-ramdomBombas()
-crearBombas()
+function prepararJuego() {
+    bombas = []
+    valorTextarea.innerHTML = valorCantidad * valorCantidad
+    dibujarGrid(valorCantidad)
+    ramdomBombas()
+    dibujarBombas()
+}
 
+function juego(item) {
+    const casillaAbierta = item.target.parentNode
+    localizar(casillaAbierta)
+    dibujarNum(fila, columna, filas, columnas, casillaAbierta)
+}
 
-cantidad.addEventListener("input", () => {
-    valorCantidad.innerHTML = cantidad.value * cantidad.value
-    dibujarGrid(cantidad.value)
-    crearBombas()
+// onload
+prepararJuego()
+// on evento
+cantidad.addEventListener("input", async () => {
+    prepararJuego()
 })
 
-/* listarCasillas().forEach(element => {
-    element.addEventListener("click", function () {
-        // comparar elementos check con array de elementos bomba
+listarCasillas().forEach(element => {
+    element.addEventListener("click", function (evento) {
+        juego(evento)
     })
-}) */
+})
+
+
+
+
+/* if (fila > 0) {
+    const arriba = document.getElementById(Number(casillaAbierta.id) - valorCantidad);
+    if (arriba) {
+        arriba.style.border = border;
+    } */
